2c0cfb34a60429a82d3ca1ff3151272cbc84b1f1
Caio Ludo Passos <caiopss99@gmail.com>
2023-02-16 01:00:12 -0300

    Fixed code

--- app.java
+++ app.java
@@ -2,15 +2,13 @@ import javax.swing.JOptionPane;
 
 public class app {
     public static void main(String[] args) {
-        //java string to int (cast)        
-        // java swing library 
-        // java javax.swing.JOptionPane
+        
         String f = "";
         int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Type the number to be used in your calculator", "Simple calc", 1));
         int p = Integer.parseInt(JOptionPane.showInputDialog(null, "1-Add \n 2- Subtraction \n 3-Multiply \n 4- Divide", "Simple calc", 1));
 
         for (int r = 1; r < 11; r++) {
-            switch (p) { //the logic is wrong, I need to fix it but I wanted to save it anyways just to remind the syntax
+            switch (p) { 
                 case 1:
                 f += n + " + " + r + " = " + (n + r + "\n");
                     break;
